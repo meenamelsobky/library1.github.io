6a004e9c64b229e81a4c1392c9d0791ef2a4f439
meenamelsobky <meenamelsobky@gmail.com>
2022-09-22 22:04:45 +0200

    display

--- test.js
+++ test.js
@@ -6,7 +6,14 @@ let productDesriptionInput = document.getElementById("productDesription");
 
 
 
-var productList = [];
+var productList ;
+if(localStorage.getItem("theproducts")==null){
+    productList =[];
+}
+else{
+    productList = JSON.parse(localStorage.getItem("theproducts"));
+    displayProduct();
+}
 
 
 function addproduct(){
@@ -18,6 +25,7 @@ function addproduct(){
     };
     productList.push(product);
     console.log(productList);
+    localStorage.setItem("theproducts",JSON.stringify(productList));
     displayProduct();
     clearForm();
 }
@@ -25,7 +33,7 @@ function addproduct(){
 function displayProduct(){
     var holder ="";
     for(var i =0;i<productList.length;i++){
-        holder+= `<tr><td>${i}</td><td>${productList[i].name}</td><td>${productList[i].price}</td><td>${productList[i].category}</td><td>${productList[i].desc}</td><td>update</td><td>delete</td></tr>`
+        holder+= `<tr><td>${i}</td><td>${productList[i].name}</td><td>${productList[i].price}</td><td>${productList[i].category}</td><td>${productList[i].desc}</td><td><button class="btn btn-warning">update</button></td><td><button class="btn btn-danger">delete</button></td></tr>`
     }
     document.getElementById("tbod").innerHTML = holder;
 };
@@ -34,4 +42,10 @@ function clearForm(){
     productPriceInput.value ="";
     productCategoryInput.value="";
     productDesriptionInput.value="";
-}
+};
+function updateProduct(){
+
+};
+function deleteProduct(){
+
+};
